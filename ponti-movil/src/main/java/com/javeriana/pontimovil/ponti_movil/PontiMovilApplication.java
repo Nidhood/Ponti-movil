7a72f6648ddf1ef3370b63edd5ac456918e274c6
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-04 20:35:41 -0500

    :tada: First commit in this awesome project 😎!!!

---
+++ ponti-movil/src/main/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.pontimovil.ponti_movil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontiMovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontiMovilApplication.class, args);
+	}
+
+}
---
+++ ponti-movil/src/test/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeriana.pontimovil.ponti_movil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontiMovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
