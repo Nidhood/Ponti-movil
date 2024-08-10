ca3c3678a13752b3e7aad0daea7351493ae8cf9d
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-09 22:01:13 -0500

    :ambulance: Reorganization of the project file structure.

--- src/main/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplication.java
+++ src/main/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplication.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class PontiMovilApplication {
 
