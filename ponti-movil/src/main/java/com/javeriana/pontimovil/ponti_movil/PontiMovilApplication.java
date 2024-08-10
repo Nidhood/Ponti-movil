75ebe4619d3cb89cc639202981d8ef7c3d0f2f46
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-09 21:52:05 -0500

    :art: Configured database connectivity using Hibernate with JDBC and JPA for persistence. Finally, added a custom startup message for the Spring Boot Server.

--- ponti-movil/src/main/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplication.java
+++ ponti-movil/src/main/java/com/javeriana/pontimovil/ponti_movil/PontiMovilApplication.java
@@ -1,13 +1,33 @@
 package com.javeriana.pontimovil.ponti_movil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class PontiMovilApplication {
 
+	protected static final Logger logger = LoggerFactory.getLogger(PontiMovilApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PontiMovilApplication.class, args);
-	}
+		logger.info("""
 
+                                                                                                               \s
+                  ╔════════════════════════════════════════════════════════════════════════════════════════════╗
+                  ║                                                                                            ║
+                  ║   ██████╗  ██████╗ ███╗   ██╗████████╗██╗        ███╗   ███╗ ██████╗ ██╗   ██╗██╗██╗       ║
+                  ║   ██╔══██╗██╔═══██╗████╗  ██║╚══██╔══╝██║        ████╗ ████║██╔═══██╗██║   ██║██║██║       ║
+                  ║   ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║ █████╗ ██╔████╔██║██║   ██║██║   ██║██║██║       ║
+                  ║   ██╔═══╝ ██║   ██║██║╚██╗██║   ██║   ██║ ╚════╝ ██║╚██╔╝██║██║   ██║╚██╗ ██╔╝██║██║       ║
+                  ║   ██║     ╚██████╔╝██║ ╚████║   ██║   ██║        ██║ ╚═╝ ██║╚██████╔╝ ╚████╔╝ ██║███████╗  ║
+                  ║   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝        ╚═╝     ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝  ║
+                  ║                                                                                            ║
+                  ╚════════════════════════════════════════════════════════════════════════════════════════════╝
+                                          🚀 APPLICATION LAUNCHED AND READY TO SERVE! 🚀                         \
+                """);
+	}
 }
