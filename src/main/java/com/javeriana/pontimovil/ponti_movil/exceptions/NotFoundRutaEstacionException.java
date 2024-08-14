397cb46697fa3e3415d7778e96d6b98f08fc7133
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-14 10:31:06 -0500

    :white_check_mark: Update Exceptions components
    Exceptions updated:
    - NotFoundRutaEstacionException

--- src/main/java/com/javeriana/pontimovil/ponti_movil/exceptions/NotFoundRutaEstacionException.java
+++ src/main/java/com/javeriana/pontimovil/ponti_movil/exceptions/NotFoundRutaEstacionException.java
@@ -1,4 +1,7 @@
 package com.javeriana.pontimovil.ponti_movil.exceptions;
 
-public class NotFoundRutaEstacionException {
+public class NotFoundRutaEstacionException extends RuntimeException {
+    public NotFoundRutaEstacionException(String message) {
+        super(message);
+    }
 }
