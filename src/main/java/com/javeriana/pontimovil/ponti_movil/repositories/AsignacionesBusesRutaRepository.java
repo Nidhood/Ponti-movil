59b9ab63fec70158cb6582aa2de2b97d81b212d2
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-18 19:50:22 -0500

    :white_check_mark: Update Templates components
    Templates updated:
    - menu.html

--- src/main/java/com/javeriana/pontimovil/ponti_movil/repositories/AsignacionesBusesRutaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javeriana.pontimovil.ponti_movil.repositories;
-
-import com.javeriana.pontimovil.ponti_movil.entities.AsignacionesBusRuta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface AsignacionesBusesRutaRepository extends JpaRepository<AsignacionesBusRuta, UUID> {
-}
