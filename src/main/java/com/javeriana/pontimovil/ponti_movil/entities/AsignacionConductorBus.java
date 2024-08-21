2351d2f9a27631155b9b371697aa34a9b3bfedd3
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-21 11:05:26 -0500

    :construction_worker: Update Entities:
    Controllers:
    - AsignacionConductorBus
    - Conductor
    - Permiso
    - TiposUsuario

--- src/main/java/com/javeriana/pontimovil/ponti_movil/entities/AsignacionConductorBus.java
+++ src/main/java/com/javeriana/pontimovil/ponti_movil/entities/AsignacionConductorBus.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -29,4 +30,34 @@ public class AsignacionConductorBus {
     @Column(name = "dia_semana", nullable = false, length = 20)
     private String diaSemana;
 
+    public AsignacionConductorBus() {}
+
+    public AsignacionConductorBus(Conductor conductor, Bus bus, String diaSemana) {
+        this.conductor = conductor;
+        this.bus = bus;
+        this.diaSemana = diaSemana;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AsignacionConductorBus that = (AsignacionConductorBus) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getConductor(), that.getConductor()) && Objects.equals(getBus(), that.getBus()) && Objects.equals(getDiaSemana(), that.getDiaSemana());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getConductor(), getBus(), getDiaSemana());
+    }
+
+    @Override
+    public String toString() {
+        return "AsignacionConductorBus{" +
+                "id=" + id +
+                ", conductor=" + conductor +
+                ", bus=" + bus +
+                ", diaSemana='" + diaSemana + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/javeriana/pontimovil/ponti_movil/entities/TiposUsuario.java
+++ src/main/java/com/javeriana/pontimovil/ponti_movil/entities/TiposUsuario.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -24,4 +25,32 @@ public class TiposUsuario {
     @Column(name = "permiso_id", nullable = false)
     private UUID permisoId;
 
+    public TiposUsuario() {}
+
+    public TiposUsuario(String tipoUsuario, UUID permisoId) {
+        this.tipoUsuario = tipoUsuario;
+        this.permisoId = permisoId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TiposUsuario that = (TiposUsuario) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getTipoUsuario(), that.getTipoUsuario()) && Objects.equals(getPermisoId(), that.getPermisoId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTipoUsuario(), getPermisoId());
+    }
+
+    @Override
+    public String toString() {
+        return "TiposUsuario{" +
+                "id=" + id +
+                ", tipoUsuario='" + tipoUsuario + '\'' +
+                ", permisoId=" + permisoId +
+                '}';
+    }
 }
