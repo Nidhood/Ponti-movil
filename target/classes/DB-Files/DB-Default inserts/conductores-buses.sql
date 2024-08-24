80dde3784d627b8806c4417b10f662e19c16b792
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-24 17:36:23 -0500

    :wastebasket: Clean code

--- target/classes/DB-Files/DB-Default inserts/conductores-buses.sql
+++ target/classes/DB-Files/DB-Default inserts/conductores-buses.sql
@@ -1,5 +1,5 @@
 INSERT INTO CONDUCTORES_BUSES_RUTAS (CONDUCTOR_ID, BUS_ID, RUTA_ID, DIA_SEMANA) VALUES
-                                                                                    ('292a35eb-8afd-4d8b-a078-2d54be058492', '5f4bdd55-7449-42b0-820e-e93ea2f4e4bc', NULL, NULL),
-                                                                                    ('9c3ce5cd-98da-4dbc-a5d5-6e7b991d632e', 'a8fee03e-be5b-401a-bab8-a3df11111bf0', NULL, NULL),
-                                                                                    ('aa28a862-d6f1-40e4-94ff-43f44fef20e9', 'c8086529-f2c2-4b8f-a97b-561c162253a4', NULL, NULL),
-                                                                                    ('b359bea2-1100-46d4-8711-bd351a785396', '5f4bdd55-7449-42b0-820e-e93ea2f4e4bc', NULL, NULL);
+                                                                                    ('292a35eb-8afd-4d8b-a078-2d54be058492', '5f4bdd55-7449-42b0-820e-e93ea2f4e4bc', NULL, 'Lunes'),
+                                                                                    ('9c3ce5cd-98da-4dbc-a5d5-6e7b991d632e', 'a8fee03e-be5b-401a-bab8-a3df11111bf0', NULL, 'Lunes'),
+                                                                                    ('aa28a862-d6f1-40e4-94ff-43f44fef20e9', 'c8086529-f2c2-4b8f-a97b-561c162253a4', NULL, 'Lunes'),
+                                                                                    ('b359bea2-1100-46d4-8711-bd351a785396', '5f4bdd55-7449-42b0-820e-e93ea2f4e4bc', NULL, 'Lunes');
