ababd779d1ef7f6fc2dff367be5b18af6c9f274f
Ivan Orozco <ivan.oroz34@gmail.com>
2024-09-23 21:04:39 -0500

    👷 Update test files

---
+++ frontend-angular/src/app/pasajero/pasajero.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasajeroComponent } from './pasajero.component';
+
+describe('PasajeroComponent', () => {
+  let component: PasajeroComponent;
+  let fixture: ComponentFixture<PasajeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PasajeroComponent]
+    });
+    fixture = TestBed.createComponent(PasajeroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
