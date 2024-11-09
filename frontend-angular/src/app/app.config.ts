ae2a319e9c9049479ce13309f615485565b423cf
Ivan Orozco <ivan.oroz34@gmail.com>
2024-11-09 15:40:29 -0500

    :rocket: Finally, authentication and role access security works !!! 😎

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,20 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { provideLottieOptions } from 'ngx-lottie';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
+import {authInterceptor} from './share/auth-interceptor.component';
+import {AuthService} from './share/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideLottieOptions({ player: () => import('lottie-web') }),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideRouter(routes),
-    provideHttpClient(),
     provideAnimations(),
     AuthGuard,
-    RoleGuard
+    RoleGuard,
+    AuthService
   ]
 };
