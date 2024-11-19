2dc02817b3f2dfc4dfc61960871af1012a9cc5a0
DavzC <davidortiz37575@gmail.com>
2024-11-18 22:59:09 -0500

    Más rapida

--- backend-springboot/src/test/java/com/javeriana/pontimovil/ponti_movil/GestionRutasSystemTest.java
+++ backend-springboot/src/test/java/com/javeriana/pontimovil/ponti_movil/GestionRutasSystemTest.java
@@ -36,8 +36,10 @@ public class GestionRutasSystemTest {
 
     @BeforeEach
     void init() {
+
+
         this.playwright = Playwright.create();
-        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         this.browserContext = browser.newContext();
         this.page = browserContext.newPage();
     }
@@ -52,134 +54,162 @@ public class GestionRutasSystemTest {
     void flujoCompletoGestionRuta() throws InterruptedException {
         // Navegar a la página de login
         page.navigate(baseUrl + "/h-login");
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Realizar login
-        page.locator("#email").fill("admin@admin.com");
-        Thread.sleep(2000);
-        page.locator("#password input").fill("adminPass");
-        Thread.sleep(2000);
+        page.waitForSelector("input#email");
+        page.locator("#email").fill("admin1@admin.com");
+        //Thread.sleep(2000);
+        page.waitForSelector("#password input");
+        page.locator("#password input").fill("123456");
+        //Thread.sleep(2000);
+        page.waitForSelector("button[type='submit']");
         page.locator("button[type='submit']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Esperar a que cargue el menú de rutas
+        page.waitForSelector("app-r-menu");
         page.navigate(baseUrl + "/r-menu");
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Hacer clic en el módulo de agregar ruta
+        page.waitForSelector("div.modulo-ruta.agregar-ruta");
         page.locator("div.modulo-ruta.agregar-ruta").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Llenar el formulario de agregar ruta
+        page.waitForSelector("#code");
         page.locator("#code").fill("RUTA001");
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         page.locator("#horaInicioInput").click();
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
+
+        page.waitForSelector("div.p-hour-picker button[aria-label='Next Hour']");
 
         for (int i = 0; i < 4; i++) {
             page.locator("div.p-hour-picker button[aria-label='Next Hour']").click();
-            Thread.sleep(500);
+            //Thread.sleep(500);
         }
         page.keyboard().press("Enter");
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
 
         // Seleccionar días
+        page.waitForSelector("p-multiSelect#dias");
         page.locator("p-multiSelect#dias").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
+        page.waitForSelector("li:has-text(\"Lunes\")");
         page.locator("li:has-text(\"Lunes\")").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
+        page.waitForSelector("li:has-text(\"Martes\")");
         page.locator("li:has-text(\"Martes\")").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         page.locator("p-multiSelect#dias").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
+        page.waitForSelector("li#pn_id_2_source_0");
         page.locator("li#pn_id_2_source_0").click();
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
 
-
+        page.waitForSelector("button[data-pc-section='sourceMoveDownButton']");
         page.locator("button[data-pc-section='sourceMoveDownButton']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
 
         page.locator("p-button[label='Crear']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
+        page.waitForSelector("button.p-button-danger:has-text('Yes')");
         page.locator("button.p-button-danger:has-text('Yes')").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Cerrar la ventana de agregar ruta
         page.locator("p-button[label='Cancelar']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Recargar la página
         page.reload();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Verificar que la nueva ruta aparece en la lista
+        page.waitForSelector("app-r-modulo-ruta .card h2");
         Locator rutas = page.locator("app-r-modulo-ruta .card h2");
         List<String> rutasCodigos = rutas.allTextContents();
         assertEquals(true, rutasCodigos.contains("RUTA001"));
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Editar la ruta agregada
+        page.waitForSelector("app-r-modulo-ruta .card:has(h2:has-text('RUTA001'))");
         page.locator("app-r-modulo-ruta .card:has(h2:has-text('RUTA001'))").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
+        page.waitForSelector("app-r-detalles-ruta .edit-btn p-button");
         page.locator("app-r-detalles-ruta .edit-btn p-button").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
+        page.waitForSelector("app-r-editar-ruta #horaFinInput");
         page.locator("#horaFinInput").click();
-        Thread.sleep(1000);
+
+        page.waitForSelector("div.p-hour-picker button[aria-label='Next Hour']");
+        //Thread.sleep(1000);
 
         for (int i = 0; i < 13; i++) {
             page.locator("div.p-hour-picker button[aria-label='Next Hour']").click();
-            Thread.sleep(500);
+            //Thread.sleep(500);
         }
 
         page.keyboard().press("Enter");
 
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
 
         page.locator("p-button[label='Guardar']").click();
-        Thread.sleep(2000);
-
+        //Thread.sleep(2000);
+        page.waitForSelector("button.p-button-danger:has-text('Yes')");
         page.locator("button.p-button-danger:has-text('Yes')").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
 
 
         page.locator("p-button[label='Cancelar']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Recargar la página
         page.reload();
+        //Thread.sleep(2000);
+        page.waitForSelector("app-r-modulo-ruta .card h2");
+        Thread.sleep(1000);
+        page.reload();
         Thread.sleep(2000);
-
         Locator horaActualizada = page.locator("app-r-modulo-ruta .card-content div:has-text('04:00 - 13:00')");
         if (!horaActualizada.isVisible()) {
             throw new RuntimeException("Error al verificar la edición de la hora.");
         }
 
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Eliminar la ruta
+        page.waitForSelector("app-r-modulo-ruta .card:has(h2:has-text('RUTA001'))");
         page.locator("app-r-modulo-ruta .card:has(h2:has-text('RUTA001'))").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
+        page.waitForSelector("app-r-detalles-ruta .edit-btn p-button");
         page.locator("app-r-detalles-ruta .edit-btn p-button").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         page.locator("app-r-editar-ruta p-button[label='Eliminar']").click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
+        page.waitForSelector("button.p-button-danger:has-text('Yes')");
         page.locator("button.p-button-danger:has-text('Yes')").click();
         Thread.sleep(2000);
 
         // Recargar la página
         page.reload();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
         // Verificar que la ruta ya no está en la lista
+        Thread.sleep(2000);
+        page.reload();
+        page.waitForSelector("app-r-modulo-ruta .card h2");
         rutasCodigos = rutas.allTextContents();
         assertEquals(false, rutasCodigos.contains("RUTA001"));
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
     }
 
 }
